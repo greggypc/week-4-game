939e8060d0995e19e0de57d2ad768853bfa6d0d2
Greg Church <greggypc@users.noreply.github.com>
2017-11-19 11:33:19 -0600

    1st round works. 2nd round gives multiple click results for each single click. Console.log'd AF searching for error

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -5,41 +5,54 @@
 $(document).ready(function() {
 
 var targetNumber = Math.floor(Math.random() * (120-19+1)) + 19;
-var num1 = 1 + Math.floor(Math.random() * 11);
-var num2 = 1 + Math.floor(Math.random() * 11);
-var num3 = 1 + Math.floor(Math.random() * 11);
-var num4 = 1 + Math.floor(Math.random() * 11);
+var num1 = Math.floor(Math.random() * (12-1+1)) + 1;
+var num2 = Math.floor(Math.random() * (12-1+1)) + 1;
+var num3 = Math.floor(Math.random() * (12-1+1)) + 1;
+var num4 = Math.floor(Math.random() * (12-1+1)) + 1;
 var scoreCount = 0;
 var wins = 0;
 var losses = 0;
 
 		function layoutGame() {
-			$("#targetNumber").html("<h1>" + targetNumber + "</h1>");
-			$("#totalWins").html("<h1>" + "Wins " + wins + "</h1>");
-			$("#totalLosses").html("<h1>" + "Losses " + losses + "</h1>");
-			$("#scoreCount").html("<h1>" + "Score count is " + scoreCount + "</h1>");
+			$("#targetNumberHolder").html("<h1>" + targetNumber + "</h1>");
+			$("#totalWinsHolder").html("<h1>" + "Wins " + wins + "</h1>");
+			$("#totalLossesHolder").html("<h1>" + "Losses " + losses + "</h1>");
+			$("#scoreCountHolder").html("<h1>" + "Score count is " + scoreCount + "</h1>");
 		};
 
 
 		function clickToPlay() {
-	 		
+	 		console.log("scoreCount " + scoreCount);
+	 		console.log("targetNumber " + targetNumber);
 			$(".crystalImg").click(function(){
 				if (this.id == "billy") {
+					console.log("num1 = " + num1)
 			    scoreCount += num1;
-			    $("#scoreCount").html("<h1>" + "Score count is " + scoreCount + "</h1>");
+			    	console.log("num1 after should be same = " + num1)
+			    $("#scoreCountHolder").html("<h1>" + "Score count is " + scoreCount + "</h1>");
 			    console.log("billy button clicked -1");
+			    console.log("scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 			    } else if (this.id == "light") {
+			    	console.log("num2 = " + num2)
 			      scoreCount += num2;
-			      $("#scoreCount").html("<h1>" + "Score count is " + scoreCount + "</h1>");
+			      console.log("num2 after should be same = " + num2)
+			      $("#scoreCountHolder").html("<h1>" + "Score count is " + scoreCount + "</h1>");
 			      console.log("light button clicked -2");
+			      console.log("scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 			    } else if (this.id == "gayle") {
+			    	console.log("num3 = " + num3)
 			      scoreCount += num3;
-			      $("#scoreCount").html("<h1>" + "Score count is " + scoreCount + "</h1>");
+			      console.log("num3 after should be same = " + num3)
+			      $("#scoreCountHolder").html("<h1>" + "Score count is " + scoreCount + "</h1>");
 			      console.log("gayle button clicked -3");
+			      console.log("scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 			    } else if (this.id == "pepsi") {
+			    	console.log("num4 = " + num4)
 			      scoreCount += num4;
-			      $("#scoreCount").html("<h1>" + "Score count is " + scoreCount + "</h1>");
+			      console.log("num4 after should be same = " + num4)
+			      $("#scoreCountHolder").html("<h1>" + "Score count is " + scoreCount + "</h1>");
 			      console.log("pepsi button clicked -4");
+			      console.log("scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 			    } gameResults();
 			    
 			    });
@@ -47,30 +60,61 @@ var losses = 0;
 			}; //end clickToPlay
 
 		function gameResults() {
+			console.log("scoreCount " + scoreCount);
 		if (scoreCount === targetNumber) {
 			console.log("about to run winGame function");
+			console.log("win in function gameResults -- scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 	        winGame();
-	    } else if (scoreCount >= targetNumber) {
+	    }else if (scoreCount >= targetNumber) {
+	    	console.log("scoreCount " + scoreCount);
 	    	console.log("about to run loseGame function");
+	    	console.log("loss in function gameResults -- scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 	        loseGame();
+	    }else {
+	    	return;
 	    }
 		};
 
 
 		function winGame() {
+		console.log("function winGame running");
+		console.log("scoreCount " + scoreCount);
+
 	    wins++;
-	    $("#totalWins").html("<h1>" + "Wins " + wins + "</h1>");
-	    console.log("function winGame ran");
+	    $("#totalWinsHolder").html("<h1>" + "Wins " + wins + "</h1>");
+	    console.log("inside winGame function gameResults -- scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 	    resetGame();
-	    clickToPlay();
+	    console.log("function winGame finished - restart");
+	   // clickToPlay();
 
 	  	};
 
 	function loseGame() {
+		console.log("function loseGame running");
+		console.log("scoreCount " + scoreCount);
 		losses++;
-	    $("#totalLosses").html("<h1>" + "Losses " + losses + "</h1>");
+	    $("#totalLossesHolder").html("<h1>" + "Losses " + losses + "</h1>");
+	    console.log("inside loseGame function gameResults -- scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
 	    resetGame();
-	    console.log("function loseGame ran");
+	    console.log("function loseGame finished - restart");
+	    //clickToPlay();
+	    };
+
+	function resetGame() {
+		console.log("function resetGame running");
+		console.log("inside resetGame function - B4 scoreCountset to 0 -- gameResults -- scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
+		scoreCount = 0;
+		$("#scoreCountHolder").html("<h1>" + "game ended - Score count is " + scoreCount + "</h1>");
+	    targetNumber = 18 + Math.floor(Math.random() * 101);
+	    $("#targetNumberHolder").html("<h1>" + targetNumber + "</h1>");
+	    var num1 = Math.floor(Math.random() * (12-1+1)) + 1;
+	    var num2 = Math.floor(Math.random() * (12-1+1)) + 1;
+	    var num3 = Math.floor(Math.random() * (12-1+1)) + 1;
+	    var num4 = Math.floor(Math.random() * (12-1+1)) + 1;
+	    console.log("inside resetGame function - AFTER scoreCountset to 0 -- gameResults -- scoreCount: " + scoreCount + " targetNumber: " + targetNumber);
+		//console.log(num1 + " " + num2 + " " + num3 + " " num4);
+	    console.log("function resetGame ran");
+	    //return;
 	    clickToPlay();
 	    };
 
